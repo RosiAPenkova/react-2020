d98a77daf13dbb9ebbfb46e878499a4409737ef6
RosiPenkova <60029198+RosiAPenkova@users.noreply.github.com>
2020-01-25 13:20:06 +0200

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import ClassComponent from './components/ClassComponent'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <ClassComponent description="Test props" subtitle="Test subtitle"/>
+    <Router>
+    <div>
+      <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+  </nav>
+  
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/profile">
+          <Profile/>
+        </Route>
+        <Route path="/users">
+          <Users/>
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/pages/Users.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Users extends React.Component {
+
+ 
+    render() {
+      return <div>
+         User Page
+      </div>
+    }
+  }
+
+
+  export default Users;
