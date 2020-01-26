c9b63ad5c650dc71c55d7f883620835cde494139
RosiAPenkova <rosipenkova1@gmail.com>
2020-01-26 13:27:42 +0200

    Introduction to redux

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
 class Users extends React.Component {
+  constructor(props) {
+    super(props);
 
- 
-    render() {
-      return <div>
-         User Page
-      </div>
-    }
+    console.log(props.movies);
+  }
+
+  componentDidMount = () => {
+    this.props.getMovies();
   }
 
 
-  export default Users;
+  render() {
+
+    return <ul className="list-group">
+              {this.props.movies.map(movie =>{
+              return <li key={movie.id} className="list-group-item">{movie.title}</li>
+              })}
+            </ul>
+            }
+          }
+          
+function getGlobalMovies() {
+  return dispatch => {
+              fetch('https://facebook.github.io/react-native/movies.json')
+                .then((response) => response.json())
+                .then((responseJson) => {
+                  console.log(responseJson);
+                  dispatch({
+                    type: "FETCHED_MOVIES",
+                    payload: responseJson.movies
+                  })
+                })
+                .catch((error) => {
+                  console.error(error);
+                });
+        };
+      }
+const mapStateToProps = state => ({movies: state.movies });
+const mapStateToDispatch = dispatch => {
+  return {
+              getMovies: () => dispatch(getGlobalMovies())
+          }
+        }
+        
+export default connect(mapStateToProps, mapStateToDispatch)(Users);
